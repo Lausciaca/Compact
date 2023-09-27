fb3707e985d211b1465ecba84fa0dbbde2cad79b
Laureano <laureanosciacaluga@gmail.com>
2023-09-27 15:17:18 -0300

    V0.1.1 Implementacion del logo giratorio

--- src/routes/inicio.jsx
+++ src/routes/inicio.jsx
@@ -5,12 +5,16 @@ import { Outlet } from "react-router-dom"
 
 function InicioPage () {
     return (
-        <>
+        <div className="h-full overflow-hidden">
             <Nav actualPage={'Inicio'}/>
-            <div id="detail">
-                <Outlet />
-            </div>
-        </>
+            
+            <main className="h-full pl-40">
+                <section className="flex justify-center items-center w-1/3 h-full">
+                    <img src="\src\assets\react.svg" alt="Logo React" className="h-4/6 logo react"/>
+                </section>
+            </main>
+
+        </div>
     )
 }
 
