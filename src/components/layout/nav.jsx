a7b029f815bb101b7b6854396e02eb02f8c716de
Laureano <laureanosciacaluga@gmail.com>
2023-09-27 15:49:14 -0300

    V0.1.4 rediseño del nav

--- src/components/layout/nav.jsx
+++ src/components/layout/nav.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 function Nav({actualPage}){
 
     return(
-        <nav className="h-16 flex justify-center p-5 bg-blue-700">
+        <nav className="h-16 flex justify-center p-5">
             <div className="flex items-center">
                 <Link to={'/'} className="titulo text-3xl font-bold">COMPACT</Link>
             </div>
